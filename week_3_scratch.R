8adca554eca05b23e3e9faf5f614b9e9cb1bfa5c
jerry-dumblauskas <jdumblauskas@gmail.com>
2016-02-15 09:46:46 -0600

    R work

---
+++ week_3_scratch.R
@@ -0,0 +1,12 @@
+set.seed(1234)
+x <- rnorm(12, rep(1:3, each = 4), 0.2)
+y <- rnorm(12, rep(c(1, 2, 1), each = 4), 0.2)
+plot(x, y, col = "blue", pch = 19, cex = 2)
+text(x + 0.05, y + 0.05, labels = as.character(1:12))
+
+dataFrame <- data.frame(x=x, y=y)
+dist(dataFrame)
+
+set.seed(143)
+dataMatrix<-as.matrix(dataFrame)[sample(1:12),]
+heatmap(dataMatrix)
