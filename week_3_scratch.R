4d0969c01a020165d3ca42166ad41978dfd9e033
jerry-dumblauskas <jdumblauskas@gmail.com>
2016-02-17 07:47:55 -0600

    meh -- scratch work following lecture

--- week_3_scratch.R
+++ week_3_scratch.R
@@ -26,3 +26,24 @@ names(kmeansObj)
 par(mar = rep(.2, 4))
 plot(x,y, col=kmeansObj$cluster, pch = 19, cex=2)
 points(kmeansObj$centers, col = 1:3, pch = 3, cex = 3, lwd = 3)
+
+
+# dim red
+
+
+set.seed(12345)
+par(mar = rep(0.2, 4))
+dataMatrix <- matrix(rnorm(400), nrow = 40)
+image(1:10, 1:40, t(dataMatrix)[, nrow(dataMatrix):1])
+
+
+set.seed(678910)
+for (i in 1:40) {
+  coinFlip <- rbinom(1, size = 1, prob = 0.5)
+  ## If coin is heads add a common pattern to that row
+  if (coinFlip) {
+    dataMatrix[i, ] <- dataMatrix[i, ] + rep(c(0, 3), each = 5)
+  }
+}
+
+heatmap(dataMatrix)
